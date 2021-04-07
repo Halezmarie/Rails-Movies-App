28aba7f2757253a6445b5ffbd495c437cfdd59a5
halezmarie <teacherhaleymarie@gmail.com>
2021-04-06 23:56:33 -0500

    added api key privately

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ENV["OMDB_KEY"]
+
+
+response = RestClient.get('')
+binding.pry
